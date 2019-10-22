a2476ceead777e91a167816ec7d73d070590df8a
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-22 22:55:03 +0200

    Setup account applications

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+    return render(request, 'accountApp/signup.html')
+
+
+def login(request):
+    return render(request, 'accountApp/login.html')
+
+
+def logout(request):
+    # TODO: Route it to home page
+    return render(request, 'accountApp/logout.html')
