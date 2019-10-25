f17b81dc89432b0aec21f641b7a07db1a9e26d58
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-25 11:37:51 +0200

    Setup media rooting

--- productHunt/urls.py
+++ productHunt/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from homeApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accountApp/', include('accountApp.urls')),
     path('product/', include('homeApp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
