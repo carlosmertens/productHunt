29b6dafc19900362498acbf93433a008f8f52638
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-25 09:51:05 +0200

    Setup create product

--- homeApp/views.py
+++ homeApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'homeApp/index.html')
+
+
+@login_required
+def create(request):
+    return render(request, 'homeApp/create.html')
